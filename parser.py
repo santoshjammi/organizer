eaa26ae4cd53101b61c1cad517c768497889a322
santoshjammi <santoshjammi@gmail.com>
2020-11-08 18:42:18 +0530

    Add files via upload

--- parser.py
+++ parser.py
@@ -1,25 +1,44 @@
 #!/usr/bin/python
 import csv , json
 
-csvFilePath = "/Users/svmss/Desktop/mywork/ContactList.csv"
-oppcsv="/Users/svmss/Desktop/mywork/OpportunityList.csv"
-jsonFilePath = "Output.json"
+conCsvFilePath = "ContactList.csv"
+oppCsvFilePath = "OpportunityList.csv"
+tasCsvFilePath = "TaskList.csv"
+jsonFilePath = "Output1.json"
 arr = []
 arr1 = []
+i=0
+k=0
 #read the csv and add the arr to a arrayn
 
-with open (csvFilePath) as csvFile, open (oppcsv) as OpportunityCsv:
+with open (conCsvFilePath) as csvFile:
     csvReader = csv.DictReader(csvFile)
-    oppReader = csv.DictReader(OpportunityCsv)
     for csvRow in csvReader:
+        #arr.append(csvRow)
+        
+        i=i+1
+        print(csvRow['Name'])
+        with open (oppCsvFilePath) as OpportunityCsv, open (tasCsvFilePath) as taskCsv:
+            oppReader = csv.DictReader(OpportunityCsv)
+            taskReader = csv.DictReader(taskCsv)
+
+            for oppRow in oppReader:
+                if (oppRow['Contact1'] or oppRow['Contact2'] )!= "":
+                    if (oppRow['Contact1'] or oppRow['Contact2']) in csvRow['Name']:
+                        print("Contact 1 is  " +oppRow['Contact1']+ " and Contact2 is "+ oppRow['Contact2'] )
+                        arr1.append(oppRow)
+
+            csvRow['Opportunities']=arr1
+            arr1=[]
+            for taskRow in taskReader:
+                if taskRow['PrimaryContact']!= "":
+                    if taskRow['PrimaryContact'] in csvRow['Name']:
+                        print("Primary Contact is  " +taskRow['PrimaryContact'])
+                        arr1.append(taskRow)
+            csvRow['tasks']=arr1                
+            arr1=[]
+
         arr.append(csvRow)
-        #print(csvRow['Name'])
-        for oppRow in oppReader:
-            arr1.append(oppRow)
-            if( (csvRow['Name'] == oppRow['Contact1']) ):
-                print("Contact 1 is  " +oppRow['Contact1']+ " and Contact2 is "+ oppRow['Contact2'] )
-
-
 # write the data to a json file
 with open(jsonFilePath, "w") as jsonFile:
     jsonFile.write(json.dumps(arr, indent = 4))
